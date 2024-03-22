6bb6f1bef3a3fdb171466bf9bba35703053aef69
cholponklv <cbaiel@mail.ru>
2024-03-22 22:43:57 +0600

    fsdaf

--- emergency/settings.py
+++ emergency/settings.py
@@ -173,7 +173,7 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
