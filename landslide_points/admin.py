5a744b414363184242ea7c125538fc8f1c6361e3
cholponklv <cbaiel@mail.ru>
2024-03-22 22:28:30 +0600

    panel

--- landslide_points/admin.py
+++ landslide_points/admin.py
@@ -3,12 +3,13 @@ from .models import Point
 # Register your models here.
 @admin.register(Point)
 class PointAdmin(admin.ModelAdmin):
+    
     list_display = ('name', 'status', 'location','speed','temperature','finished','depth')
     list_filter = ('status', 'location')
     search_fields = ('name',)
     fieldsets = (
         (None, {
-            'fields': ('name', 'status', 'location')
+            'fields': ('name', 'status', 'location','speed','temperature','finished','depth')
         }),
         ('GPS координаты', {
             'fields': ('longitude', 'latitude'),
