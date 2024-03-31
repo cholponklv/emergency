3749f777bc29e740f16b6b8662de51199416557a
cholponklv <cbaiel@mail.ru>
2024-03-31 16:07:32 +0600

    aefaа

--- project/serializers.py
+++ project/serializers.py
@@ -10,6 +10,12 @@ class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ['id','project', 'file', 'description','created_at']
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        request = self.context.get('request')
+        if request:
+            data['file'] = request.build_absolute_uri(instance.file.url)
+        return data
 
 class ProjectSerializer(serializers.ModelSerializer):
     photos = PhotoSerializer(many=True, read_only=True)
