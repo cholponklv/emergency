15a1cb7ce93221c5c57f961d0e341c74a1c35782
cholponklv <cbaiel@mail.ru>
2024-04-29 16:10:44 +0600

    fasdfsa

--- tender/serializers.py
+++ tender/serializers.py
@@ -1,18 +1,5 @@
 from rest_framework import serializers
 from .models import Tender, TenderResult,DocumentTender,DocumentResultTender
-
-class TenderResultSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TenderResult
-        fields = ('result_description_ru','result_description_kg','result_description_en',)
-
-class TenderSerializer(serializers.ModelSerializer):
-    result = TenderResultSerializer(required=False)
-
-    class Meta:
-        model = Tender
-        fields = ('id', 'name_ru','name_kg','name_en', 'created_at', 'deadline', 'description_ru','description_kg','description_en',  'status', 'result')
-
 class DocumentTenderSerializer(serializers.ModelSerializer):
     class Meta:
         model = DocumentTender
@@ -34,3 +21,16 @@ class DocumentResultSerializer(serializers.ModelSerializer):
         if request:
             data['file'] = request.build_absolute_uri(instance.file.url)
         return data
+
+class TenderResultSerializer(serializers.ModelSerializer):
+    documents_result = DocumentTenderSerializer(many=True, read_only=True)
+    class Meta:
+        model = TenderResult
+        fields = ['result_description_ru','result_description_kg','result_description_en','documents_result']
+
+class TenderSerializer(serializers.ModelSerializer):
+    result = TenderResultSerializer(required=False)
+    documents_tender = DocumentResultSerializer(many=True, read_only=True)
+    class Meta:
+        model = Tender
+        fields = ['id', 'name_ru','name_kg','name_en', 'created_at', 'deadline', 'description_ru','description_kg','description_en',  'status', 'result','documents_tender']
